811794190d7a11da6847d79fb136d59b93d68b72
willm <willmoiser@gmail.com>
2021-11-12 10:13:32 -0600

    Changed DatasetImporter.py to take directories. Unzipped exampleDownloadedDataset.zip, and deleted the zip file.

--- src/DatasetImporter.py
+++ src/DatasetImporter.py
@@ -3,6 +3,7 @@ import os
 import cv2
 from zipfile import ZipFile
 import tensorflow as tf
+import numpy as np
 
 
 class DatasetImporter:
@@ -21,53 +22,127 @@ class DatasetImporter:
         'image/encoded': tf.io.FixedLenFeature([], tf.string),
     }
 
+    # tf.io.parse_single_example(example_proto, feature_description)
     @staticmethod
-    def load_dataset(filename):
-        print("loading record names: ", filename)
+    def _parse_dataset_function(example_proto):
+        # Parse the input tf.train.Example proto using the dictionary above.
+        return tf.io.parse_single_example(example_proto, DatasetImporter.feature_description)
 
-        filenames = [filename]
-        raw_dataset = tf.data.TFRecordDataset(filenames)
+    @staticmethod
+    def get_dataset_size(recordfile):
+        return sum(1 for _ in tf.data.TFRecordDataset(recordfile))
+
+    @staticmethod
+    def load_record_image(parsed_data):
+        # Get dimensions
+        raw_height = tf.cast(parsed_data['image/height'], tf.int32).numpy()
+        raw_width = tf.cast(parsed_data['image/width'], tf.int32).numpy()
+
+        # Load image
+        image_tf = tf.io.decode_raw(parsed_data['image/encoded'], tf.uint8).numpy()
+        image_raw = cv2.imdecode(image_tf, cv2.IMREAD_COLOR)
+        return image_raw.reshape((raw_height, raw_width, 3))
+
+    @staticmethod
+    def load_record_label(parsed_data):
+        box_xmin = tf.cast(parsed_data['image/object/bbox/xmin'], tf.float32).numpy()
+        box_xmax = tf.cast(parsed_data['image/object/bbox/xmax'], tf.float32).numpy()
+        box_ymin = tf.cast(parsed_data['image/object/bbox/ymin'], tf.float32).numpy()
+        box_ymax = tf.cast(parsed_data['image/object/bbox/ymax'], tf.float32).numpy()
+        label = tf.cast(parsed_data['image/object/class/label'], tf.int64).numpy()
+        ul = np.array([box_xmin, box_ymin])  # uppper left
+        br = np.array([box_xmax, box_ymax])  # bottom right
+        return ul, br, label
+
+    @staticmethod
+    def convert_to_yolo_output(ul, br, label):
+        output_tensor = np.zeros((7, 7, 25))
+        center = (ul + br) / 2.0
+        print(center)
+        center_grid = center * np.array([7, 7])
+        print(center_grid)
+        coords = center_grid.astype(np.int32)
+        print(coords)
+
+        classes = np.zeros((20))
+        classes[label] = 1.0
+
+        output_tensor[coords[1], coords[0], 0:2] = center_grid - coords
+
+        output_tensor[coords[1], coords[0], 2:4] = br - ul
+        output_tensor[coords[1], coords[0], 4] = 1.0
+        output_tensor[coords[1], coords[0], 5:25] = classes
+
+        return output_tensor
+
+    @staticmethod
+    def _parse_record_file(filename: str):
+
+        raw_dataset = tf.data.TFRecordDataset([filename])
+        breakpoint()
+
+        parsed_dataset = raw_dataset.take(12).map(DatasetImporter._parse_dataset_function)
+
+
 
         # Sample load of 10 features
-        for raw_record in raw_dataset.take(10):
-            example = tf.train.Example()
-            example.ParseFromString(raw_record.numpy())
+        for parsed_data in parsed_dataset:
+            # Load bounding box and label info
+            ul, br, label = DatasetImporter.load_record_label(parsed_data)
 
-            sourceFile = open('features.txt', 'w')
-            print(example, file=sourceFile)
-            sourceFile.close()
-            # print(example)
+            output_tensor = DatasetImporter.convert_to_yolo_output(ul, br, label)
 
-            parsed_data = tf.io.parse_single_example(raw_record, DatasetImporter.feature_description)
+            breakpoint()
 
-            raw_height = tf.cast(parsed_data['image/height'], tf.int32)
-            raw_width = tf.cast(parsed_data['image/width'], tf.int32)
-            image_tf = tf.io.decode_raw(parsed_data['image/encoded'], tf.uint8)
+            image = DatasetImporter.load_record_image(parsed_data)
 
-            print(raw_height)
-            print(raw_width)
+            # Convert bounding box to pixel values for ploting
+            ul_px = (ul * np.array([image.shape[1], image.shape[0]])).astype(np.int32)
+            br_px = (br * np.array([image.shape[1], image.shape[0]])).astype(np.int32)
+            cv2.rectangle(image, ul_px, br_px, (255, 0, 0), 2)
 
-            image_tf = tf.reshape(image_tf, [raw_height, raw_width, 3])
-            image_tf = tf.image.resize_images(image_tf, size=[self.height, self.width])
-
-            with tf.Session() as sess:
-                image = sess.run(image_tf)
+            image = DatasetImporter.ResizeCrop(image, [448, 448, 3])
 
             cv2.imshow("tf", image)
             cv2.waitKey(0)
 
     @staticmethod
-    def _parse_record_file(file_contents: io.BytesIO) -> list:
-        example = tf.train.Example()
-        example.ParseFromString(raw_record.numpy())
-        raw_dataset = tf.data.TFRecordDataset(file_contents.read())
-        parsed_data = tf.io.parse_single_example(raw_dataset, DatasetImporter.feature_description)
-        breakpoint()
+    def ResizeCrop(img, dim_out):
+        dim_img = img.shape
+
+        ratio_height = dim_out[0] / dim_img[0]
+        ratio_width = dim_out[1] / dim_img[1]
+
+        # determine which ratio to use
+        ratio = ratio_width
+        if abs(1.0 - ratio_height) < abs(1.0 - ratio_width):
+            ratio = ratio_height
+
+        # scaled the image
+        scaled = cv2.resize(img, dsize=(0, 0), fx=ratio, fy=ratio)
+        dim_scaled = scaled.shape
+
+        # determine offsets (one of these will be 0)
+        shiftx = int((dim_scaled[1] - dim_out[1]) / 2)
+        shifty = int((dim_scaled[0] - dim_out[0]) / 2)
+
+        output = np.zeros((dim_out[0], dim_out[1], 3), np.uint8)
+        if shiftx < 0 or shifty < 0:
+            # fill black
+            shiftx = abs(shiftx)
+            shifty = abs(shifty)
+            output[shifty:(shifty + dim_scaled[0]), shiftx:(shiftx + dim_scaled[1]), :] = scaled
+        else:
+            # crop
+            output = scaled[shifty:(shifty + dim_out[0]), shiftx:(shiftx + dim_out[1]), :]
+
+        return output
 
     @staticmethod
-    def _parse_pbtxt_file(file_contents: io.BytesIO) -> dict:
+    def _parse_pbtxt_file(filename:str) -> dict:
         # reads in the data from the file object as a string
-        data = file_contents.read().decode()
+        with open(filename,'r') as f:
+            data = f.read().decode()
 
         # splits the data into sections
         data = data.split('\n')
@@ -123,17 +198,22 @@ class DatasetImporter:
         return False
 
     @staticmethod
-    def load(file_name: str):
-        z = ZipFile(file_name, 'r')
-        record_files = []
-        for file in z.filelist:
-            if DatasetImporter._is_pbtxt(file.filename):
-                label = DatasetImporter._parse_pbtxt_file(z.open(file))
-            elif DatasetImporter._is_record_file(file.filename):
+    def absoluteFilePaths(directory):
+        for dirpath, _, filenames in os.walk(directory):
+            for f in filenames:
+                yield os.path.abspath(os.path.join(dirpath, f))
 
-                record_files.append(DatasetImporter._parse_record_file(z.open(file)))
+    @staticmethod
+    def load(folder_name: str):
+
+        record_files = [file for file in DatasetImporter.absoluteFilePaths(folder_name)]
+        for file in record_files:
+            if DatasetImporter._is_pbtxt(file):
+                label = DatasetImporter._parse_pbtxt_file(file)
+                print("parsed .pbtxt")
+            elif DatasetImporter._is_record_file(file):
+                record_files.append(DatasetImporter._parse_record_file(file))
             else:
                 # sanity check on the file
                 breakpoint()
                 raise ValueError("The File is Malformed")
-        z.close()
--- src/main.py
+++ src/main.py
@@ -4,8 +4,7 @@ from src.DatasetImporter import DatasetImporter
 
 
 def main():
-    # DatasetImporter.load(r"..\datasetExamples\exampleDownloadedDataset.zip")
-    DatasetImporter.load_dataset(r"C:\Users\willm\Downloads\Ex\train_dataset.record-00000-00001")
+    DatasetImporter.load(r"..\datasetExamples\Dataset1")
     sys.exit(0)
 
 
